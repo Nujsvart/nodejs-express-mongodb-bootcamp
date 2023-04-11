53eb1b796e3dacaf8a6655c9c601898da9746dcd
Esra C <kampfar@gmail.com>
2023-04-11 09:50:47 +0300

    URL module, routing

--- 1-node-farm/starter/httpModule.js
+++ 1-node-farm/starter/httpModule.js
@@ -1,9 +1,24 @@
 const http = require("http");
+const url = require("url"); //? URL module.
 
 //* createServer function ile server olusturulur. req ve res paremetresi alir. request geldiginde res.end() ile server'dan yanit doner.
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server");
+  // console.log(req.url); //* her res geldiginde hangi url'de oldugunu gosterir.
+
+  //? Buyuk projelerde routing islemi zor olabilir. Bu yuzden routing islemi icin genelde Express kullanilir.
+
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("Hello from OVERVIEW!");
+  } else if (pathName === "/product") {
+    res.end("Hello from PRODUCT!");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 //* olusturudugumuz serveri dinlemek icin .listen() fonksiyonu kullanilir. server'dan request bekler. 3 parametre alir. 1: port, 2: ip adress. 3: callback. (request geldiginde calisir.)
