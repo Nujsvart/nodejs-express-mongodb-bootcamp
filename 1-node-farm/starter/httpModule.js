822dc4d65802281872aa364964fefa7c23c3dc89
Esra C <kampfar@gmail.com>
2023-04-11 10:28:40 +0300

    building a very simple API

--- 1-node-farm/starter/httpModule.js
+++ 1-node-farm/starter/httpModule.js
@@ -1,8 +1,13 @@
 const http = require("http");
+const fs = require("fs");
 const url = require("url"); //? URL module.
 
-//* createServer function ile server olusturulur. req ve res paremetresi alir. request geldiginde res.end() ile server'dan yanit doner.
+//* route'da /api'ye girildiginde datayi her seferinde okumak yerine, top-level code olarak bir kez senkron bir sekilde okutup variable'a kaydederiz. bloklamaz.
+//* kaydedilen var'i serverda sadece request geldiginde res.end(data) ile gonderebiliriz.
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+// const dataObj = JSON.parse(data)
 
+//* createServer function ile server olusturulur. req ve res paremetresi alir. request geldiginde res.end() ile server'dan yanit doner.
 const server = http.createServer((req, res) => {
   // console.log(req.url); //* her res geldiginde hangi url'de oldugunu gosterir.
 
@@ -13,6 +18,9 @@ const server = http.createServer((req, res) => {
     res.end("Hello from OVERVIEW!");
   } else if (pathName === "/product") {
     res.end("Hello from PRODUCT!");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
